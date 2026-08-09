68b9c2b19eb2ab3c2dea1f1187dbceee5c92cde9
SoulSolomon21 <sekamattesoul@gmail.com>
2026-08-09 21:52:43 +0300

    fix: export ProductAggregate and update append method call in ProductEventJournal

--- packages/domain/src/scratch.ts
+++ packages/domain/src/scratch.ts
@@ -2,7 +2,7 @@ import { Effect, Schema } from "effect"
 import * as AggregateRoot from "./AggregateRoot.js"
 import type { Message } from "./Message.js"
 
-const ProductAggregate = AggregateRoot.AggregateRoot({
+export const ProductAggregate = AggregateRoot.AggregateRoot({
   aggregateRootName: "products"
 })
 
@@ -44,10 +44,6 @@ export class MemberJoined extends Schema.TaggedClass<MemberJoined>()(
   MemberAggregate.Event({})
 ) {}
 
-interface EventJournal<A extends AggregateRoot.AggregateRoot<string>, Events> {
-  append(event: Events): Effect.Effect<void, never, void>
-}
-
 function makeEventSourcedAggregate<A extends AggregateRoot.AggregateRoot<string>>(aggregateRoot: A) {
   return <Events extends ReadonlyArray<Message.AnyForAggregate<A>>>(...events: Events) =>
   <R>(
@@ -66,6 +62,6 @@ const ProductEventJournal = makeEventSourcedAggregate(ProductAggregate)(ProductN
   }
 )
 
-const A = ProductEventJournal.append([ProductNameChanged])
+const A = ProductEventJournal.append(ProductNameChanged)
 
 const memberAggregateJournal = makeEventSourcedAggregate(MemberAggregate)(MemberJoined)
